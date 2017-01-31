15506852aa9701e4a9dddf5531231624aaba80e5
Peter Squicciarini <petersquicciarini@Daphne.local>
2017-01-31 13:43:32 -0500

    finished writing the code

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by petersquicciarini on 1/31/17.
+ */
+
+var express = require('express');
+var app = express();
+
+app.get('/:input', function(req, res) {
+    var timeInput = req.params.input,
+        unix = null,
+        natural = null,
+        validNat = /(?:^(?:January|February|March|April|May|June|July|August|September|October|November|December)\s\d{1,2},\s\d{4}$)/.test(timeInput),
+        validUnix = /(?:^\d+$)/.test(timeInput);
+    if (validNat || validUnix) {
+        var date = new Date(validUnix ? +timeInput : timeInput);
+        console.log(date);
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        unix = date.getTime();
+        natural = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+    }
+    res.send(JSON.stringify({ unix: unix, natural: natural }));
+});
+
+app.listen(8080);
